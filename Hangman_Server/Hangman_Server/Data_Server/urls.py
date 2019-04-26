2622da71a18cbc53b68f19436b88110638fe2acf
Soumic Shekhar <soumic.aoyon@northsouth.edu>
2019-04-26 20:37:15 +0600

    Avatar Added -> User Table, Achievement table added,

--- Hangman_Server/Hangman_Server/Data_Server/urls.py
+++ Hangman_Server/Hangman_Server/Data_Server/urls.py
@@ -3,7 +3,7 @@ from . import views
 from rest_framework import routers
 
 router = routers.DefaultRouter()
-router.register('Data_Server', views.user_View)
+router.register('Data_Server', views.user_View, views.achievement_View)
 
 urlpatterns = [
     path('', include(router.urls))
